44cb6e8412be2b54cfc5b16d2971c3f03af6f394
Ilyxxxa <example@gmail.com>
2022-02-08 20:55:25 +0300

    adaptiv

--- script/main.js
+++ script/main.js
@@ -7,6 +7,28 @@ $(function() {
         slidesToScroll: 4,
         prevArrow: '<img class="slider__arrow slider__arrow-left " src="images/arrow-left.svg" alt="">',
         nextArrow: '<img class="slider__arrow slider__arrow-right " src="images/arrow-right.svg" alt="">',
+        responsive: [{
+                breakpoint: 850,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 525,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 400,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
     $('.gallery__slider').slick({
         centerMode: true,
@@ -15,5 +37,28 @@ $(function() {
         slidesToScroll: 1,
         prevArrow: '<img class="gallery__arrow gallery__arrow-left " src="images/gallery__arrow-left.png" alt="">',
         nextArrow: '<img class="gallery__arrow gallery__arrow-right " src="images/gallery__arrow-right.png" alt="">',
+        responsive: [{
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+
+                }
+            },
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    centerMode: false
+
+                }
+            }
+        ]
+    });
+    $('.header__btn').on('click', function() {
+        $('.header__nav-items').toggleClass('header__nav-item-active');
     });
 });
